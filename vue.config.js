c70ed283c3a8c109a2ffdd4a26e8ac23ddaab317
Adrien Emery <adrien.emery@gmail.com>
2018-10-31 20:07:04 -0700

    Add vue s3 deploy plugin

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,28 @@
 module.exports = {
   lintOnSave: false,
+
   devServer: {
     useLocalIp: false,
-    public: 'http://localhost:8080',
+    public: 'http://localhost:8081',
+  },
+
+  pluginOptions: {
+    s3Deploy: {
+      awsProfile: 'default',
+      region: 'us-west-2',
+      bucket: 'auvcontrol.com',
+      createBucket: false,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      uploadConcurrency: 5,
+      pluginVersion: '3.0.0'
+    }
   }
 }
